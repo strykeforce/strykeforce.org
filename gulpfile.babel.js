5891bd256d1292738651f124a5461c84c839b6fa
Jeff Hutchison <jeff@jeffhutchison.com>
2018-05-15 09:31:33 -0400

    Lint CSS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -111,3 +111,15 @@ function buildSite(cb, options, environment = "development") {
     }
   });
 }
+
+gulp.task('lint-css', function lintCssTask() {
+  const gulpStylelint = require('gulp-stylelint');
+
+  return gulp
+    .src('src/**/*.css')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});
