58e1545c42ddee9358efc1c48bad96b2593ae3bb
Jeff Hutchison <jeff@jeffhutchison.com>
2018-11-02 07:17:36 -0400

    Add exclusion rule

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,12 @@ module.exports = {
       'This is the Stryke Force Web Site. Stryke Force is a community FIRST robotics team based in Kalamazoo, Michigan. We are FRC 2767, FTC 7228 and FTC 8553.',
   },
   plugins: [
-    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        exclude: ['/howto/*'],
+      },
+    },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
