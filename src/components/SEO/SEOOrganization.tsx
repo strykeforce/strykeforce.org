236d42b88798072e1f9aa27e3ff47542c3b33f97
Jeff Hutchison <jeff@jeffhutchison.com>
2018-11-03 09:58:50 -0400

    Add 15106 to search results

--- src/components/SEO/SEOOrganization.tsx
+++ src/components/SEO/SEOOrganization.tsx
@@ -17,7 +17,7 @@ export const LOGO = `${URL}logo.png`
 export const ORGANIZATION_ID = `${URL}#organization`
 export const STRYKE_FORCE = `Stryke Force 4-H FIRST Robotics Team 2767 is
     a community FIRST robotics team based in Kalamazoo,
-    Michigan. We are FRC 2767, FTC 7228 and FTC  8553.`
+    Michigan. We are FRC 2767, FTC 7228, FTC  8553 and FTC 15106.`
 export const ORGANIZATION = {
   '@context': 'http://schema.org',
   '@id': ORGANIZATION_ID,
@@ -31,6 +31,7 @@ export const ORGANIZATION = {
     'https://facebook.com/2767strykeforce',
     'https://github.com/strykeforce',
     'https://instagram.com/2767strykeforce',
+    'https://strykeforce.smugmug.com',
   ],
   url: URL,
 }
