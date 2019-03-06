da7a707816d883687810b8f0f77f7df5fdea0cd2
Jeff Hutchison <jeff@jeffhutchison.com>
2019-03-06 10:22:07 -0500

    Update partners page

--- src/components/Partner/PartnerCard.tsx
+++ src/components/Partner/PartnerCard.tsx
@@ -1,16 +1,45 @@
+import Img from 'gatsby-image'
 import React from 'react'
 import styled from 'react-emotion'
 import { CardWrapper } from '../CardGallery/CardWrapper'
 
+const PartnerDiv = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 4em;
+  width: auto;
+
+  @media (max-width: 480px) {
+    flex-flow: column;
+    width: auto;
+  }
+`
+
 const Logo = styled.img`
-  width: 100%;
+  max-width: 100%;
   height: auto;
 `
 
-export const PartnerCard: React.SFC<{ item: Sponsor }> = ({ item }) => (
+const Blurb = styled.p`
+  text-align: left;
+  padding-left: 2em;
+  max-width: 50%;
+
+  @media (max-width: 480px) {
+    padding-left: 0;
+    max-width: 100%;
+  }
+`
+
+export const PartnerCard: React.SFC<{ item: Partner }> = ({ item }) => (
   <CardWrapper>
-    <a href={item.url} target="_blank" rel="noopener noreferrer">
-      <Logo src={item.logo.childImageSharp.original.src} alt={item.name} />
-    </a>
+    <PartnerDiv>
+      <a href={item.url} target="_blank" rel="noopener noreferrer">
+        <Logo src={item.logo.childImageSharp.original.src} />
+      </a>
+      <Blurb>{item.text}</Blurb>
+    </PartnerDiv>
   </CardWrapper>
 )
