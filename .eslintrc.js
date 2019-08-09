798c6bbbf42b36ae6e999b0bf2512401ee7cfdbf
Jeff Hutchison <jeff@jeffhutchison.com>
2019-08-09 12:16:38 -0400

    Add eslint with TS plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,51 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/indent': ['error', 2],
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+  },
+}
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "commonjs": true,
+//         "es6": true,
+//         "node": true
+//     },
+//     "extends": "eslint:recommended",
+//     "parserOptions": {
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "single"
+//         ],
+//         "semi": [
+//             "error",
+//             "never"
+//         ]
+//     }
+// };
