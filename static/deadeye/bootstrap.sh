1e24f5b31b91e2c9237c0b2b9d759ceed0c7a85b
Jeff Hutchison <jeff@jeffhutchison.com>
2022-04-05 08:00:53 -0400

    Apply shellcheck suggestions

--- static/deadeye/bootstrap.sh
+++ static/deadeye/bootstrap.sh
@@ -18,22 +18,22 @@ case "$HOSTNAME" in
 		IP=10.27.67.13
 		;;
 	*)
-		echo Unrecognized hostname: $HOSTNAME
+		echo "Unrecognized hostname: $HOSTNAME"
 		exit 1
 		;;
 esac
 
-nmcli connection modify $UUID \
+nmcli connection modify "$UUID" \
 	connection.id ether-eth0 \
 	ipv4.method manual \
 	ipv4.addresses $IP/24 \
 	ipv4.gateway 10.27.67.1 \
 	ipv4.dns 10.27.67.1
 
-nmcli connection down $UUID > /dev/null
-nmcli connection up $UUID > /dev/null
+nmcli connection down "$UUID" > /dev/null
+nmcli connection up "$UUID" > /dev/null
 
-echo Configured $HOSTNAME with static ip address $IP
+echo "Configured $HOSTNAME with static ip address $IP"
 
 echo '%sudo    ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/local
 echo Allow sudo without password for Ansible
