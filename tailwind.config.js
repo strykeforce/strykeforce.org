ac02ab399e0e313165f2f868b3114f2f52c2ea39
Jeff Hutchison <jeff@jeffhutchison.com>
2022-12-27 13:18:52 -0500

    Add stryke force colors to Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,42 @@
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
-
+const colors = require('tailwindcss/colors')
 module.exports = {
-    content: ["./website/templates/*.html", "./website/templates/**/*.html"],
+    content: ["*.html"],
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            yellow: {
+                200: '#FFE8A4',
+                300: '#FFD047',
+                400: '#FFD047',
+                500: '#F3BB17',
+                DEFAULT: '#F3BB17',
+                600: '#D8A716',
+                700: '#BD9213'
+            },
+            blue: {
+                100: '#BBE4FF',
+                200: '#91D3FF',
+                300: '#5EB1E9',
+                400: '#3292D2',
+                500: '#0E76BC',
+                DEFAULT: '#0E76BC',
+                600: '#0D619A',
+                700: '#0B4C77',
+                800: '#093755',
+                900: '#062133',
+            }
+        },
         extend: {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
         },
-    }, plugins: [
-        require('@tailwindcss/typography'),
-    ],
+    },
+    plugins: [require('@tailwindcss/typography'),],
 }
