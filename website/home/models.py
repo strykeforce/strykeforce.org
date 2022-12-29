b102cab69da900a3a58c922ec4a753112db5fd87
Jeff Hutchison <jeff@jeffhutchison.com>
2022-12-29 17:19:44 -0500

    AboutPage -> ContentPage with RichTextField

--- website/home/models.py
+++ website/home/models.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
 from django.db import models
-from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
-from wagtail.fields import StreamField
-from wagtail.images.blocks import ImageChooserBlock
 from wagtail.models import Page
 
 
@@ -33,16 +30,11 @@ class PartnersPage(Page):
     ]
 
 
-class AboutPage(Page):
-    body = StreamField(
-        [
-            ("heading", blocks.CharBlock(form_classname="title")),
-            ("paragraph", blocks.RichTextBlock()),
-            ("image", ImageChooserBlock()),
-        ],
-        use_json_field=True,
-    )
+class ContentPage(Page):
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel("intro"),
         FieldPanel("body"),
     ]
