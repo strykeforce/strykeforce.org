cf1aba4aac2359513aef79027a4784f27e34857f
Jeff Hutchison <jeff@jeffhutchison.com>
2023-01-05 15:28:05 -0500

    Add events app

---
+++ website/events/models.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+from django.db import models
+from django.db.models import CharField
+from django.db.models import DateField
+from django.db.models import URLField
+from django.utils import timezone
+from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldRowPanel
+from wagtail.admin.panels import MultiFieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Page
+
+
+class EventIndexPage(Page):
+    body = models.TextField(
+        blank=True,
+        max_length=1000,
+        help_text="Text to describe the page",
+    )
+
+    def events(self):
+        return EventPage.objects.descendant_of(self).live().order_by("date_start")
+
+    def future_events(self):
+        return self.events().filter(date_start__gt=timezone.now())
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    subpage_types = [
+        "events.EventIndexPage",
+        "events.EventPage",
+    ]
+
+
+class EventPage(Page):
+    date_start = DateField()
+    date_end = DateField()
+    tba_url = URLField(blank=True, verbose_name="TBA URL")
+    event_url = URLField(blank=True, verbose_name="Event URL")
+    venue = CharField(max_length=100)
+    street = CharField(max_length=100)
+    city = CharField(max_length=100)
+    state = CharField(max_length=2)
+    zip = CharField(max_length=10)
+    body = RichTextField(blank=True)
+
+    class Meta:
+        indexes = [models.Index(fields=["date_start"])]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("date_start"),
+                        FieldPanel("date_end"),
+                    ],
+                ),
+                FieldPanel("tba_url"),
+                FieldPanel("event_url"),
+            ],
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("venue"),
+                FieldPanel("street"),
+                FieldRowPanel(
+                    [
+                        FieldPanel("city"),
+                        FieldPanel("state"),
+                        FieldPanel("zip"),
+                    ],
+                ),
+            ],
+        ),
+        FieldPanel("body"),
+    ]
+
+    subpage_types: list[str] = []
+    parent_page_types = ["events.EventIndexPage"]
--- website/home/models.py
+++ website/home/models.py
@@ -7,6 +7,7 @@ from wagtail.models import Page
 
 from website.blog.models import BlogIndexPage
 from website.blog.models import BlogPage
+from website.events.models import EventIndexPage
 
 
 class HomePage(Page):
@@ -23,9 +24,11 @@ class HomePage(Page):
     def get_context(self, request, **kwargs):
         context = super().get_context(request)
         context["blog_posts"] = BlogPage.objects.descendant_of(self).live().order_by("-date")[0:3]
-        blog_index_page = BlogIndexPage.objects.descendant_of(self).live().first()
+        blog_index_page = BlogIndexPage.objects.child_of(self).live().first()
         context["blog_title"] = blog_index_page.title
         context["blog_intro"] = blog_index_page.body
+        event_index_page = EventIndexPage.objects.child_of(self).live().first()
+        context["event_index"] = event_index_page
         return context
 
     content_panels = Page.content_panels + [
