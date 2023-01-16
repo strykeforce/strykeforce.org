a94e9b1b6c7bb49d18f2e09b3d81bbb445292e55
Jeff Hutchison <jeff@jeffhutchison.com>
2023-01-16 08:31:55 -0500

    Add redli

--- deploy/configuration.nix
+++ deploy/configuration.nix
@@ -7,7 +7,7 @@
 
   systemd.services.amazon-init.enable = false;
 
-  # environment.systemPackages = with pkgs; [ ];
+  environment.systemPackages = with pkgs; [ redli ];
   programs = {
     git.enable = true;
     vim.defaultEditor = true;
