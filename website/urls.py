1f56021f4dbd8f83a54429cfbd918e26aa971692
Jeff Hutchison <jeff@jeffhutchison.com>
2023-01-17 20:07:12 -0500

    Add robots.txt

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.views.generic.base import TemplateView
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -21,6 +22,10 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
     path("sentry-debug/", trigger_error),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
 
 
