0629fc341e7afbc5fd77466bcadc63ec1aa82193
Jeff Hutchison <jeff@jeffhutchison.com>
2023-01-19 07:03:28 -0500

    Add favicons

--- website/urls.py
+++ website/urls.py
@@ -26,6 +26,13 @@ urlpatterns = [
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
+    path(
+        "manifest.webmanifest",
+        TemplateView.as_view(
+            template_name="manifest.webmanifest",
+            content_type="application/json",
+        ),
+    ),
 ]
 
 
