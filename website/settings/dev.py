f50fc9865d922944e3fd329456dc99c12ce32eed
Jeff Hutchison <jeff@jeffhutchison.com>
2023-01-24 12:19:54 -0500

    Turn off caching

--- website/settings/dev.py
+++ website/settings/dev.py
@@ -35,7 +35,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        # "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
         "LOCATION": BASE_DIR / "cache",
     },
     "renditions": {
