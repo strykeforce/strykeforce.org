ab3f65dda004f75457de69f9cef69463d68c5797
Jeff Hutchison <jeff@jeffhutchison.com>
2024-03-08 08:42:04 -0500

    Bump version to 4.1.4

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     let
-      version = "4.1.3"; # also update pyproject.toml
+      version = "4.1.4"; # also update pyproject.toml
     in
     flake-utils.lib.eachDefaultSystem
       (system:
